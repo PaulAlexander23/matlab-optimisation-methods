9fc776088b2d6e0957a712c41d5233109e54bf3b
Paul Alexander <paulalexander23@live.co.uk>
2019-03-25 17:20:20 +0000

    Add new test functions and code for fsolve with Jacobian.

--- main.m
+++ main.m
@@ -1,13 +1,18 @@
 %MAIN Script to dummy run the various optimisation methods
 
-n = 128; % Probably best if even
+n = 2; % Probably best if even
 x0=ones(n,1);
 x0(1:2:end) = -1.2 * ones(n/2,1);
 
-solver = 'lmgb';
-problem = @(x) testFunction(x,n);
+solver = 'newton';
+%problem = @(x) testFunction(x,n);
+problem = @testFunction3;
+
 
 x = feval(solver,problem,x0);
+
+%options = optimoptions('fsolve','SpecifyObjectiveGradient',true);
+%x = feval(solver,problem,x0,options);
 
 xe = norm(x - 1,'inf');
 
@@ -23,4 +28,21 @@ function f = testFunction(x,n)
     f = zeros(n,1);
     f(1:2:end) = (x(1:2:end)-1) + (x(2:2:end)-1).^2;
     f(2:2:end) = (x(1:2:end)-1) .* (x(2:2:end)-1);
+end
+
+function [f,J] = testFunction2(x) % size(x) = [2,1];
+    f = [(x(1)-1)^2 + (x(1)-1)^2;(x(1)-1)*(x(2)-1)];
+    if nargout > 1
+        J = [2*(x(1)-1),2*(x(2)-1);x(2)-1,x(1)-1];
+    end
+end
+
+function [f, J] = testFunction3(x)
+    eta = 0.1;
+    f = [x(1)-1 + eta*(x(1)-1).*(x(2)-1);...
+        x(2)-1 - eta*(x(1)-1).*(x(2)-1)];
+    if nargout > 1
+        J = [1 + eta*(x(2)-1), eta*(x(1)-1);
+            - eta*(x(2)-1), 1 + eta*(x(1)-1)];
+    end
 end
