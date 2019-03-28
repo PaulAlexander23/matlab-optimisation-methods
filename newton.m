9796284908f7bc9ab54ba5e6ef0f99efc2f9c818
Paul Alexander <paulalexander23@live.co.uk>
2019-03-28 12:15:46 +0000

    Increase max number of iterations and remove testing output.

--- newton.m
+++ newton.m
@@ -3,12 +3,11 @@ function x = newton(fun,x0)
     x = x0;
     n = 1;
     err = 1;
-    while err > 1e-6 && n <= 20
+    while err > 1e-7 && n <= 400
         [f,J] = fun(x);
         x = x - J\f;
 
         err = norm(f);
-        
         n = n + 1;
     end
 end
